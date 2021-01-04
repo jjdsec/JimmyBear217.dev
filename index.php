2bf2fa2331d79a50f794e48e540e94bd089fff90
JimmyBear217 <jimmybear217@gmail.com>
2021-01-04 14:29:35 -0800

    corrected projects icon on home page

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <section>
             <ul class="icons-horizontal">
                 <li><a href="https://github.com/jimmybear217" target="_blank" title="Open my Github Profile"><img src="/assets/img/github1600.png" height="72" width="72" alt="GitHub"></a></li>
-                <li><a href="/projects" title="Open my Project Portfolio"><img src="/assets/img/projects.svg" height="72" width="72" alt="Projects"></a></li>
+                <li><a href="/projects" title="Open my Project Portfolio"><img src="/assets/img/projects.png" height="72" width="72" alt="Projects"></a></li>
             </ul>
         </section>
 <?php include_once(__DIR__ . "/assets/inc/footer.inc.php"); ?>
