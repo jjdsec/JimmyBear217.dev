eb463a675c888ec7a7983c0a66b4ed62134d8712
JimmyBear217 <jimmybear217@gmail.com>
2021-01-03 22:20:48 -0800

    updated

--- projects/repo/file_explorer/FileExplorer/api/show_file.php
+++ projects/repo/file_explorer/FileExplorer/api/show_file.php
@@ -17,6 +17,11 @@
         die("Missing token");
     }
 
+    if (substr($path, 0, strlen($chroot)) != $chroot && $username == $_COOKIE["explorer_username"]) {
+        header("Content-Type: text/json", true, 403);
+        die(json_encode(array("status" => "error","message" => "this user cannot access this directory")));
+    }
+
     $mime = mime_content_type($path);
     header("Content-Type: " . $mime);
     //var_dump(pathinfo($path));
