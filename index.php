8bf8cfa929def6a1aff7a335378273c82610822d
JimmyBear217 <jimmybear217@gmail.com>
2021-01-03 16:03:55 -0800

    setting up style

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php include_once(__DIR__ . "/assets/inc/header.inc.php"); ?>
         <header>
             <h1>JimmyBear217 - Web Engineer</h1>
-            <p>Using web technologies to create tomorrow's applications</p>
+            <p>Using today's web technologies to create tomorrow's applications</p>
         </header>
         <section>
             <ul class="icons-horizontal">
-                <li><a href="https://github.com/jimmybear217"><img src="/assets/img/github.png" alt="GitHub"></a></li>
-                <li><a href="/projetcs"><img src="/assets/img/projects.png" alt="Projects"></a></li>
+                <li><a href="https://github.com/jimmybear217" title="Open my Github Profile"><img src="/assets/img/github1600.png" height="72" width="72" alt="GitHub"></a></li>
+                <li><a href="/projetcs" title="Open my Project Portfolio"><img src="/assets/img/projects.svg" height="72" width="72" alt="Projects"></a></li>
             </ul>
         </section>
 <?php include_once(__DIR__ . "/assets/inc/footer.inc.php"); ?>
