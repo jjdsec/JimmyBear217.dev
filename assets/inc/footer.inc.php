67da5984e09747d0644b4aa81d9a53d5a706305c
JimmyBear217 <jimmybear217@gmail.com>
2021-01-04 22:51:21 -0800

    improved style of footer

--- assets/inc/footer.inc.php
+++ assets/inc/footer.inc.php
@@ -1,5 +1,5 @@
         <footer>
-            Made with &#9829; by <a href="https://www.jimmybear217.com">JimmyBear217</a> in Los Angeles, California
+            Made with &#9829; by <a href="https://www.jimmybear217.com">JimmyBear217</a><br class='reverseTitleDash600' /> in Los Angeles, California
         </footer>
     </body>
 </html>
