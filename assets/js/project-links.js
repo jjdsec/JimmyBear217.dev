be6695438467bca1d2a2d8106c7d02fb63c4c4af
JimmyBear217 <jimmybear217@gmai.com>
2021-01-17 17:07:25 -0800

    corrected project name and path

--- assets/js/project-links.js
+++ assets/js/project-links.js
@@ -1,6 +1,6 @@
 function loadLinks() {
     repo = "/projects/repo";
-    project = document.getElementsById("project-links").getAttribute("data-project");
+    project = document.getElementById("project-links").getAttribute("data-project");
     manifest = repo + "/" + project + "/project-manifest.json";
 
     fetch(manifest)
@@ -9,6 +9,7 @@ function loadLinks() {
                 return response.json();
             } else {
                 return {
+                    name: "",
                     links: []
                 };
             }
@@ -19,13 +20,10 @@ function loadLinks() {
                 switch (link.type) {
                     case "start":
                         var a = document.createElement("a");
-                        a.setAttribute("href", "/projects/" + repo + "/" + project.dir + "/" + link.value);
-                        a.setAttribute("title", "Open " + project.name);
-                        a.setAttribute("target", "_blank");
+                        a.setAttribute("href", repo + "/" + project + "/" + link.value);
+                        a.setAttribute("title", "Launch " + responseJson.name);
                         var img = document.createElement("img");
                         img.setAttribute("src", "/assets/img/open-in-browser.svg");
-                        img.setAttribute("height", "24");
-                        img.setAttribute("width", "24");
                         img.setAttribute("alt", "Launch");
                         a.appendChild(img);
                         li.appendChild(a);
@@ -34,12 +32,9 @@ function loadLinks() {
                     case "github":
                         var a = document.createElement("a");
                         a.setAttribute("href", link.value);
-                        a.setAttribute("title", "See " + project.name + " on github");
-                        a.setAttribute("target", "_blank");
+                        a.setAttribute("title", "See " + responseJson.name + " on github");
                         var img = document.createElement("img");
                         img.setAttribute("src", "/assets/img/github1600.png");
-                        img.setAttribute("height", "24");
-                        img.setAttribute("width", "24");
                         img.setAttribute("alt", "GitHub");
                         a.appendChild(img);
                         li.appendChild(a);
@@ -48,17 +43,17 @@ function loadLinks() {
                     case "web":
                         var a = document.createElement("a");
                         a.setAttribute("href", link.value);
-                        a.setAttribute("title", "Visit the website for " + project.name);
-                        a.setAttribute("target", "_blank");
+                        a.setAttribute("title", "Visit the website for " + responseJson.name);
                         var img = document.createElement("img");
                         img.setAttribute("src", "/assets/img/world-wide-web.svg");
-                        img.setAttribute("height", "24");
-                        img.setAttribute("width", "24");
                         img.setAttribute("alt", "Website");
                         a.appendChild(img);
                         li.appendChild(a);
                         break;
                 }
+                a.setAttribute("target", "_blank");
+                img.setAttribute("height", "72");
+                img.setAttribute("width", "72");
                 document.getElementById("project-links").appendChild(li);
                 if (document.getElementById("project-links").classList.contains("loading"))
                     document.getElementById("project-links").classList.remove("loading");
